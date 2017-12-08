86d1b5a31cf5a4989d0227bbc6205aed422a427b
Supanida Manothirakul <doraemon.plang@gmail.com>
2017-12-08 15:08:23 +0700

    Update Webservice_Test.go

--- Webservice_Test.go
+++ Webservice_Test.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"bytes"
-	"encoding/xml"
 	"fmt"
-	"log"
 	"net/http"
 	"testing"
-
-	"github.com/webconnex/xmlutil"
 	//"net/http"
 )
+
+type areYouThereResponse struct {
+	Three string
+}
 
 func TestMain(t *testing.T) {
 	buf := new(bytes.Buffer)
 
 	buf.WriteByte('\n')
 	buf.WriteString(generateRequestContent())
-	
+
 	// Print request
 	bs := buf.Bytes()
 	bs = bytes.Replace(bs, []byte{'>', '<'}, []byte{'>', '\n', '<'}, -1)
@@ -30,26 +30,6 @@ func TestMain(t *testing.T) {
 	}
 	fmt.Printf("Test 1 :%v\n", r)
 	fmt.Printf("Test 2 :%v\n", err)
-
-	dec := x.NewDecoder(r.Body)
-	find := []xml.Name{
-		xml.Name{"", "areYouThereResponse"},
-		xml.Name{"http://www.w3.org/2003/05/soap-envelope", "Fault"},
-	}
-	var start *xml.StartElement
-	if start, err = dec.Find(find); err != nil {
-		log.Fatal(err)
-		fmt.Printf("Resp : %#v\n\n", err)
-	}
-	if start.Name.Local == "Fault" {
-		log.Fatal("Fault!") // Here you can decode a Soap Fault
-	}
-	var resp areYouThereResponse
-	if err := dec.DecodeElement(&resp, start); err != nil {
-		log.Fatal(err)
-		fmt.Printf("Resp : %#v\n\n", err)
-	}
-	fmt.Printf("Resp : %#v\n\n", err)
 }
 func generateRequestContent() string {
 	const getTemplate = `
